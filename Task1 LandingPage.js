9406c45198edfe21f890ee731f8b0d9c8b4eb17e
fatmaabdelmonem <fatmaabdelmonem03@gmail.com>
2023-09-24 20:12:03 +0300

    Add files via upload

---
+++ Task1 LandingPage.js
@@ -0,0 +1,10 @@
+let imG=document.getElementById('C');
+function phonee(phone)
+{
+    imG.src=phone;
+}
+let cont=document.querySelector('body');
+function colors(colorr,fcolorr)
+{
+    cont.style.background=colorr;
+}
